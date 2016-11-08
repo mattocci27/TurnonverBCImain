a213f217c139d8e8a53b392f78a1ed0c96bbe09c
Masatoshi Katabuchi <mattocci27@gmail.com>
2016-11-07 22:15:06 -0500

    Feeley's gamm fig

--- fig/gam_fig.R
+++ fig/gam_fig.R
@@ -21,10 +21,14 @@ time <- c(1982, 1985, 1990, 1995, 2000, 2005, 2010)
 # rep_ind <- sapply(WSG.ind, length)
 
 
-fig_dat <- data_frame(WSG = c(unlist(WSG100), unlist(WSG20)),
-  Moist = c(unlist(Moist100), unlist(Moist20)),
-  Convex = c(unlist(convex100), unlist(convex20)),
-  Slope = c(unlist(slope100), unlist(slope20)),
+fig_dat <- data_frame(WSG = c(unlist(WSG100) - WSG100[[1]],
+  unlist(WSG20) - WSG20[[1]]),
+  Moist = c(unlist(Moist100) - Moist100[[1]],
+    unlist(Moist20) - Moist20[[1]]),
+  Convex = c(unlist(convex100) - convex100[[1]],
+    unlist(convex20) - convex20[[1]]),
+  Slope = c(unlist(slope100) - slope100[[1]],
+    unlist(slope20) - slope20[[1]]),
   Time = c(rep(time, each = 50),
     rep(time, each = 1250)),
   size = rep(c("1ha", "0.04ha"), c(50*7, 1250*7)))
@@ -70,25 +74,32 @@ for (i in 1:7){
   convex100.rm[[i]] <- com.mean.ab(D100m[[i]][,!(colnames(D100m[[1]]) %in% c("PIPECO","POULAR"))],trait,"sp.convex.mean")
 }
 
+for (i in 1:7){
+  WSG20.rm[[i]] <- com.mean.ab(D20m[[i]][,!(colnames(D20m[[1]]) %in% c("PIPECO","POULAR"))],trait,"WSG")
+  Moist20.rm[[i]] <- com.mean.ab(D20m[[i]][,!(colnames(D20m[[1]]) %in% c("PIPECO","POULAR"))],trait,"Moist")
+  slope20.rm[[i]] <- com.mean.ab(D20m[[i]][,!(colnames(D20m[[1]]) %in% c("PIPECO","POULAR"))],trait,"sp.slope.mean")
+  # convex20.rm[[i]] <- com.mean.ab(D20m[[i]][,!(colnames(D20m[[1]]) %in% c("ALSEBL","SWARS1"))],trait,"sp.convex.mean")
+  convex20.rm[[i]] <- com.mean.ab(D20m[[i]][,!(colnames(D20m[[1]]) %in% c("PIPECO","POULAR"))],trait,"sp.convex.mean")
+}
 
-moge <- data.frame(WSG = unlist(WSG100),
-                   Moist = unlist(Moist100),
-                   slope = unlist(slope100),
-                   convex = unlist(convex100),
+moge <- data.frame(WSG = unlist(WSG100) - WSG100[[1]],
+                   Moist = unlist(Moist100) - Moist100[[1]],
+                   slope = unlist(slope100) - slope100[[1]],
+                   convex = unlist(convex100) - convex100[[1]],
                    site = as.factor(rep(1:50,7)),
                    Time)
 
 
 
-moge100r <- data.frame(WSG = unlist(WSG100.rm),
-                   Moist = unlist(Moist100.rm),
-                   slope = unlist(slope100.rm),
-                   convex = unlist(convex100.rm),
+moge100r <- data.frame(WSG = unlist(WSG100.rm) - WSG100.rm[[1]],
+                   Moist = unlist(Moist100.rm) - Moist100.rm[[1]],
+                   slope = unlist(slope100.rm) - slope100.rm[[1]],
+                   convex = unlist(convex100.rm) - convex100.rm[[1]],
                    site = rep(1:50,7),
                    Time)
 
 
-# r1.r100<- gamm(WSG ~  s(Time,k=4), random = list(site=~1), data=moge100r, correlation = corAR1(form = ~Time))
+r1.r100<- gamm(WSG ~  s(Time,k=4), random = list(site=~1), data=moge100r, correlation = corAR1(form = ~Time))
 # r2.r100<- gamm(Moist ~  s(Time,k=4), random = list(site=~1), data=moge100r, correlation = corAR1(form = ~Time))
 # r3.r100<- gamm(convex ~  s(Time,k=4), random = list(site=~1), data=moge100r, correlation = corAR1(form = ~Time))
 r4.r100 <- gamm(slope ~  s(Time,k=4), random = list(site=~1), data=moge100r, correlation = corAR1(form = ~Time))
@@ -98,18 +109,39 @@ r2_100 <- gamm(Moist ~  s(Time,k=4), random = list(site=~1), data=moge, correlat
 r3_100 <- gamm(convex ~  s(Time,k=4), random = list(site=~1), data=moge, correlation = corAR1(form = ~Time))
 r4_100 <- gamm(slope ~  s(Time,k=4), random = list(site=~1), data=moge, correlation = corAR1(form = ~Time))
 
+res1_100 <- predict(r1_100$gam, se.fit=T)
+res1r100 <- predict(r1.r100$gam, se.fit=T)
+res2_100 <- predict(r2_100$gam, se.fit=T)
+res2r100 <- predict(r2.r100$gam, se.fit=T)
+res3_100 <- predict(r3_100$gam, se.fit=T)
+res3r100 <- predict(r3.r100$gam, se.fit=T)
 
 res4_100 <- predict(r4_100$gam, se.fit=T)
 res4r100 <- predict(r4.r100$gam, se.fit=T)
 
 
-fig_dat_n <- data_frame(WSG = c(unlist(WSG100.rm), unlist(WSG20.rm)),
-  Moist = c(unlist(Moist100.rm), unlist(Moist20.rm)),
-  Convex = c(unlist(convex100.rm), unlist(convex20.rm)),
-  Slope = c(unlist(slope100.rm), unlist(slope20.rm)),
+# fig_dat_n <- data_frame(WSG = c(unlist(WSG100.rm), unlist(WSG20.rm)),
+#   Moist = c(unlist(Moist100.rm), unlist(Moist20.rm)),
+#   Convex = c(unlist(convex100.rm), unlist(convex20.rm)),
+#   Slope = c(unlist(slope100.rm), unlist(slope20.rm)),
+#   Time = c(rep(time, each = 50),
+#   rep(time, each = 1250)),
+#   size = rep(c("1ha", "0.04ha"), c(50*7, 1250*7))) %>%
+#   tidyr::gather(., "trait", "val", 1:4) %>%
+#   mutate(trait = factor(trait, levels = c("WSG", "Moist", "Convex", "Slope"))) %>%
+#   mutate(trait2 = factor(trait, labels = c("Wood~density ~(g~cm^{-3})", "Moist", "Convexity~(m)", "Slope~(degrees)")))
+
+fig_dat_n <- data_frame(WSG = c(unlist(WSG100.rm) - WSG100.rm[[1]],
+  unlist(WSG20.rm) - WSG20.rm[[1]]),
+  Moist = c(unlist(Moist100.rm) - Moist100.rm[[1]],
+    unlist(Moist20.rm) - Moist20.rm[[1]]),
+  Convex = c(unlist(convex100.rm) - convex100.rm[[1]],
+    unlist(convex20.rm) - convex20.rm[[1]]),
+  Slope = c(unlist(slope100.rm) - slope100.rm[[1]],
+    unlist(slope20.rm) - slope20.rm[[1]]),
   Time = c(rep(time, each = 50),
-  rep(time, each = 1250)),
-  size = rep(c("1ha", "0.04ha"), c(50*7, 1250*7))) %>%
+    rep(time, each = 1250)),
+  size = rep(c("1ha", "0.04ha"), c(50*7, 1250*7)))  %>%
   tidyr::gather(., "trait", "val", 1:4) %>%
   mutate(trait = factor(trait, levels = c("WSG", "Moist", "Convex", "Slope"))) %>%
   mutate(trait2 = factor(trait, labels = c("Wood~density ~(g~cm^{-3})", "Moist", "Convexity~(m)", "Slope~(degrees)")))
@@ -123,6 +155,7 @@ ggplot(., aes(x = jitter(Time), y = val) ) +
   theme_bw() +
   xlab("Time") +
   ylab("Trait values")
+
 
 temp1 <- fig_dat4 %>%
   mutate(obs = "ori")
@@ -149,22 +182,7 @@ fig_dat5 <- fig_dat4 %>%
 
 ##########
 
-names(r1data100)[2:3] <- c("obs", "rm")
-names(r2data100)[2:3] <- c("obs", "rm")
-names(r3data100)[2:3] <- c("obs", "rm")
-names(r4data100)[2:3] <- c("obs", "rm")
 
-gam_dat <- bind_rows(r1data100, r2data100, r3data100, r4data100) %>%
-  mutate(Trait = rep(c("WSG", "Moist", "Convex", "Slope"), each = 21)) %>%
-  tidyr::gather(., "obs", "val", 2:3)
-
-ggplot(gam_dat, aes(x = time, y = val, colour = obs)) +
-  geom_point() +
-  geom_smooth() +
-  facet_wrap(~ Trait, scale = "free")
-
-r1data100 %>% tidyr::gather(., "obs", "val", 2:3) %>%
-  tidyr::gather(., "se", "val2", 2:4) %>% unique
 
 r1data100 <- data.frame(time = spline(r1_100$gam$model$Time, res1_100$fit)$x,
           mean = spline(r1_100$gam$model$Time, res1_100$fit)$y,
@@ -239,6 +257,22 @@ r4data100_r <- data.frame(time = spline(r4.r100$gam$model$Time, res4r100$fit)$x,
           obs = "tr")
 
 moge4 <- bind_rows(r4data100, r4data100_r)
+#
+# names(r1data100)[2:3] <- c("obs", "rm")
+# names(r2data100)[2:3] <- c("obs", "rm")
+# names(r3data100)[2:3] <- c("obs", "rm")
+# names(r4data100)[2:3] <- c("obs", "rm")
+gam_dat <- bind_rows(r1data100, r2data100, r3data100, r4data100) %>%
+  mutate(Trait = rep(c("WSG", "Moist", "Convex", "Slope"), each = 21)) %>%
+  tidyr::gather(., "obs", "val", 2:3)
+
+ggplot(gam_dat, aes(x = time, y = val, colour = obs)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~ Trait, scale = "free")
+
+r1data100 %>% tidyr::gather(., "obs", "val", 2:3) %>%
+  tidyr::gather(., "se", "val2", 2:4) %>% unique
 
 
 moge <- bind_rows(moge1, moge2, moge3, moge4) %>%
@@ -290,14 +324,14 @@ temp <- bind_rows(fig_dat4, moge6) %>%
 
 
 
-temp2 <- temp %>% filter(size == "1ha")
+# temp2 <- temp %>% filter(size == "1ha")
 
 #last version
-pdf("~/Dropbox/MS/TurnoverBCI/fig/fig2_new.pdf", width = 4.77, height = 4.77)
-ggplot(filter(temp2, is.na(est_mean) == TRUE)) +
+pdf("~/Dropbox/MS/TurnoverBCI/fig/fig2_new.pdf", width = 6, height = 12)
+ggplot(filter(temp, is.na(est_mean) == TRUE)) +
     geom_point(aes(x = jitter(Time),
     y = val), size = 0.8, alpha = 0.4) +
-    facet_wrap( ~ trait2, scale = "free",
+    facet_wrap(trait2 ~ size, scale = "free",
     labeller = labeller(trait2 = label_parsed),
     switch = NULL, ncol = 2) + # geom_smooth() +
     theme_bw() +
